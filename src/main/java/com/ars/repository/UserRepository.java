4e7852c9bc5fdee88b689ebf21d740029915c413
Abbas <abbeekhan786@gmail.com>
2025-02-27 14:26:27 +0500

    added: -checks -Exceptions

--- src/main/java/com/ars/repository/UserRepository.java
+++ src/main/java/com/ars/repository/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
     User findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 
 
 }
--- src/main/java/com/ars/service/impl/UserServiceImpl.java
+++ src/main/java/com/ars/service/impl/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ars.service.impl;
 
 import com.ars.dto.UserDTO;
+import com.ars.exceptions.ApiException;
 import com.ars.model.User;
 import com.ars.mapper.UserMapper;
 import com.ars.repository.UserRepository;
 import com.ars.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,16 +19,28 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     public UserDTO getUserById(int userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ApiException("User not found ID: " + userId));
         return UserMapper.toDTO(user);
     }
 
     public UserDTO saveUser(UserDTO userDTO) {
+        if(userRepository.existsByUsername(userDTO.getName())){
+            throw  new IllegalArgumentException("Username already exists!");
+        }
+        if(userRepository.existsByEmail(userDTO.getEmail())){
+            throw  new IllegalArgumentException("Email already exists!");
+        }
         User user = UserMapper.toEntity(userDTO);
         user = userRepository.save(user);
         return UserMapper.toDTO(user);
     }
     public List<UserDTO> addMultipleUsers(List<UserDTO> userDTOList) {
+        if(userDTOList == null || userDTOList.isEmpty() || !StringUtils.hasText(userDTOList.getFirst().getName())){
+            throw  new ApiException("invalid request");
+        }
+        if(userRepository.existsByUsername(userDTOList.get(0).getName())){
+            throw new ApiException("Username already exists!");
+        }
         List<User> users = userDTOList.stream()
                 .map(UserMapper::toEntity)
                 .collect(Collectors.toList());
